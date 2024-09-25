01f5f14e6a0fd9a9d3555d8bfa9a1b514ea2b184
Gnemasime <smesihlentshangase@gmail.com>
2024-09-25 08:17:37 +0200

    first commit

---
+++ EmployeeManagement/Models/Employee.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name ="Employee Name")]
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string Address { get; set; }        
+        public DateTime? RecordCreatedOn { get; set; }
+
+    }
+}
